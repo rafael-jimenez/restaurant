effa84cabdd6ff880388b3119570e9170725099f
Rafael Jimenez <rapahel.andres@icloud.com>
2021-12-06 15:49:11 -0500

    First Commit

---
+++ src/main/java/restaurant/survey/restaurant/RestaurantApplication.java
@@ -0,0 +1,13 @@
+package restaurant.survey.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantApplication.class, args);
+    }
+
+}
